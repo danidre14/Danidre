0fc7d85535fc46f1785b6f441319c2085fff751a
Carlonn Rivers <danidre14@gmail.com>
2020-07-28 15:13:18 -0400

    Automatically Deletes Unverified Users Longer than 2 Days

--- routes/signup.js
+++ routes/signup.js
@@ -9,7 +9,7 @@ const Token = require('../models/token');
 const testEmail = false;
 
 router.get('/', checkNotAuthenticated, async (req, res) => {
-    let vars = {cPage: "signup", searchOptions: req.query};
+    let vars = { cPage: "signup", searchOptions: req.query };
     vars.uMessage = req.flash('uMessage');
     vars.pMessage = req.flash('pMessage');
     vars.p2Message = req.flash('p2Message');
@@ -17,11 +17,11 @@ router.get('/', checkNotAuthenticated, async (req, res) => {
     vars.bodyUname = req.flash('bodyUname') || '';
     vars.bodyEmail = req.flash('bodyEmail') || '';
     vars.title = "Sign Up";
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         try {
-            const user = await User.findOne({username: new RegExp("^" + req.user.username + "$", "i")}, 'username profileImage profileImageType');
+            const user = await User.findOne({ username: new RegExp("^" + req.user.username + "$", "i") }, 'username profileImage profileImageType');
             vars.user = user;
-        } catch {}
+        } catch { }
     }
     res.render('signup/index', vars);
 });
@@ -30,13 +30,13 @@ router.post('/', checkNotAuthenticated, validateInfomation, checkUserExists, cre
 
 //Verify Prompt Route
 router.get('/v', checkNotAuthenticated, async (req, res) => {
-    let vars = {cPage: "secret", searchOptions: req.query};
+    let vars = { cPage: "secret", searchOptions: req.query };
     vars.title = "Verify Account";
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         try {
-            const user = await User.findOne({username: new RegExp("^" + req.user.username + "$", "i")}, 'username profileImage profileImageType');
+            const user = await User.findOne({ username: new RegExp("^" + req.user.username + "$", "i") }, 'username profileImage profileImageType');
             vars.user = user;
-        } catch {}
+        } catch { }
     }
     vars.description = "Check your email for a link to verify your account. Check your spam folders if you can't find any email.";
     res.render('misc/blank', vars);
@@ -44,13 +44,13 @@ router.get('/v', checkNotAuthenticated, async (req, res) => {
 
 //Normal Confimation Page Route
 router.get('/verify', checkNotAuthenticated, async (req, res) => {
-    let vars = {cPage: "signup", searchOptions: req.query};
+    let vars = { cPage: "signup", searchOptions: req.query };
     vars.title = "Verify Account";
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         try {
-            const user = await User.findOne({username: new RegExp("^" + req.user.username + "$", "i")}, 'username profileImage profileImageType');
+            const user = await User.findOne({ username: new RegExp("^" + req.user.username + "$", "i") }, 'username profileImage profileImageType');
             vars.user = user;
-        } catch {}
+        } catch { }
     }
     res.render('signup/verify', vars);
 });
@@ -58,24 +58,24 @@ router.get('/verify', checkNotAuthenticated, async (req, res) => {
 //Create Confimation Page Route
 router.post('/verify', checkNotAuthenticated, async (req, res) => {
     try {
-        const token = await Token.findOne({token:req.body.token});
+        const token = await Token.findOne({ token: req.body.token });
         //Check if token exists
-        if(!token) {
-            req.flash('outsert', {message: 'The activation link does not exist, or may have expired.'});
+        if (!token) {
+            req.flash('outsert', { message: 'The activation link does not exist, or may have expired.' });
             return res.redirect('verify/resend'); //their token may have expired
         }
 
         //Check if token matches account email
-        const user = await User.findOne({_id: token._userId, email:req.body.email});
-        if(!user) {
-            req.flash('outsert', {message: 'Unable to find an account with that email. Correct the information, or sign up today.'});
+        const user = await User.findOne({ _id: token._userId, email: req.body.email });
+        if (!user) {
+            req.flash('outsert', { message: 'Unable to find an account with that email. Correct the information, or sign up today.' });
             return res.redirect('../signup'); //user does not exist
         }
 
         //Check if user is verified
         await token.remove();
-        if(user.isVerified) {
-            req.flash('outsert', {message: 'Account already verified. You can sign in.', note: true});
+        if (user.isVerified) {
+            req.flash('outsert', { message: 'Account already verified. You can sign in.', note: true });
             return res.redirect('../signin'); //user already verified
         }
 
@@ -83,23 +83,34 @@ router.post('/verify', checkNotAuthenticated, async (req, res) => {
         user.isVerified = true;
         await user.save();
 
-        req.flash('outsert', {message: 'Account verified. You may now sign in.', note: true});
+        req.flash('outsert', { message: 'Account verified. You may now sign in.', note: true });
         res.redirect('../signin'); //Please log in
-        
+
     } catch (err) {
         console.log("Message:", err.message);
-        req.flash('outsert', {message: 'An error has occured. Please try again, or contact support.'});
+        req.flash('outsert', { message: 'An error has occured. Please try again, or contact support.' });
         res.redirect('../signup');
     }
 });
 
+async function deleteUnverifiedUsers() {
+    try {
+        let twoDaysAgo = new Date();
+        twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+        twoDaysAgo = twoDaysAgo.getTime();
+
+        await User.deleteMany({ isVerified: false, "createdAt": { $lt: twoDaysAgo } });
+    } catch { }
+}
+
 async function checkUserExists(req, res, next) {
     try {
+        await deleteUnverifiedUsers();
         //look for user
-        const user = await User.findOne({username: new RegExp("^" + req.body.username + "$", "i")});
+        const user = await User.findOne({ username: new RegExp("^" + req.body.username + "$", "i") });
 
         //if user does not exist
-        if(!user) {
+        if (!user) {
             return next(); //proceed to create user
         }
 
@@ -113,38 +124,38 @@ async function checkUserExists(req, res, next) {
         */
 
         //if user verified
-        if(user.isVerified) {
-            req.flash('outsert', {message: 'Username unavailable.'});
+        if (user.isVerified) {
+            req.flash('outsert', { message: 'Username unavailable.' });
             req.flash('bodyUname', req.body.username || '');
             req.flash('bodyEmail', req.body.email || '');
             return res.redirect('/signup');
         }
-        
+
         //if user not verified, look for token
-        const token = await Token.findOne({_userId:user._id});
+        const token = await Token.findOne({ _userId: user._id });
 
         //if token does not exist
-        if(!token) {
+        if (!token) {
             // Create a verification token
-            const newToken = new Token({_userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+            const newToken = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
             await newToken.save();
 
             // Send the email for resend token
             const mailOptions = getMailOptions(user.username, user.email, req.headers.host, newToken.token);
-            
+
             sendMail(mailOptions, user.email);
 
-            req.flash('outsert', {message: `A token has been resent to ${user.email}. Check your email to verify your account.`});
+            req.flash('outsert', { message: `A token has been resent to ${user.email}. Check your email to verify your account.` });
             return res.redirect('/signup/v');
         }
 
 
         //if token exists
-        
+
         //look for user email
-        if(user.email === req.body.email) {//email exists
+        if (user.email === req.body.email) {//email exists
             //check email for verification token
-            req.flash('outsert', {message: 'Account already registered. Check your email for the verification token.'});
+            req.flash('outsert', { message: 'Account already registered. Check your email for the verification token.' });
             return res.redirect('/signin');
         } else {//emal does not exist
             //save new email
@@ -152,7 +163,7 @@ async function checkUserExists(req, res, next) {
             await user.save();
 
             // Create a verification token
-            const newToken = new Token({_userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+            const newToken = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
             await newToken.save();
 
             // Send the email for resend token
@@ -160,12 +171,12 @@ async function checkUserExists(req, res, next) {
 
             sendMail(mailOptions, user.email);
 
-            req.flash('outsert', {message: `A token has been sent to ${user.email}. Check your email to verify your account.`});
+            req.flash('outsert', { message: `A token has been sent to ${user.email}. Check your email to verify your account.` });
             return res.redirect('/signup/v');
         }
     } catch (e) {
         console.log("Message:", e.message);
-        req.flash('outsert', {message: 'Error Occurred.'});
+        req.flash('outsert', { message: 'Error Occurred.' });
         return res.redirect('/signup');
     }
 }
@@ -181,28 +192,28 @@ async function createUser(req, res) {
             password: hashedPassword
         });
 
-        if(process.env.NODE_ENV !== 'production' && !testEmail) {
+        if (process.env.NODE_ENV !== 'production' && !testEmail) {
             user.isVerified = true; //verified by default if not in production
             await user.save();
-            req.flash('outsert', {message: `Development account ${user.email} created. Sign in with username ${user.username}.`, note: true});
+            req.flash('outsert', { message: `Development account ${user.email} created. Sign in with username ${user.username}.`, note: true });
             return res.redirect('/signin');
         }
 
         await user.save();
         // Create a verification token
-        const token = new Token({_userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+        const token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
         await token.save();
 
         // Send the email
         const mailOptions = getMailOptions(user.username, user.email, req.headers.host, token.token);
 
         sendMail(mailOptions, user.email);
-        
-        req.flash('outsert', {message: `A token has been sent to ${user.email}. Check your email to verify your account.`});
+
+        req.flash('outsert', { message: `A token has been sent to ${user.email}. Check your email to verify your account.` });
         res.redirect('/signup/v');
     } catch (e) {
         console.log("Message:", e.message);
-        req.flash('outsert', {message: 'An error has occured. Please report this issue or try again.'});
+        req.flash('outsert', { message: 'An error has occured. Please report this issue or try again.' });
         res.redirect('/signup');
     }
 }
@@ -214,23 +225,23 @@ function validateInfomation(req, res, next) {
     //username validation
     const username = req.body.username;
     let uMessage = "";
-    if(username.length < 4 || username.length > 15) {
+    if (username.length < 4 || username.length > 15) {
         uMessage += "-Must be 4-15 characters long";
         error = true;
     } else {
-        if(username.charAt(0).match(/^[a-z]+$/ig) === null) {
+        if (username.charAt(0).match(/^[a-z]+$/ig) === null) {
             uMessage += "-Username must start with a letter\n";
             error = true;
-        } else if(username.match(/^[a-z][a-z\d]+$/ig) === null) {
+        } else if (username.match(/^[a-z][a-z\d]+$/ig) === null) {
             uMessage += "-Symbols/Spaces not allowed";
             error = true;
-        } 
+        }
     }
-    
+
     //password validation
     const pName = req.body.password;
     let pMessage = "";
-    if(pName.length < 8) {
+    if (pName.length < 8) {
         pMessage += "-Password must be 8 or more characters\n";
         error = true;
     }
@@ -238,11 +249,11 @@ function validateInfomation(req, res, next) {
     //     pMessage += "-Password cannot contain symbols or spaces\n";
     //     error = true;
     // }
-    if(pName.search(/\d/) === -1) {
+    if (pName.search(/\d/) === -1) {
         pMessage += "-Must contain at least one number\n";
         error = true;
     }
-    if(pName.search(/[A-Z]/) === -1) {
+    if (pName.search(/[A-Z]/) === -1) {
         pMessage += "-Must contain at least one uppercase letter\n";
         error = true;
     }
@@ -250,7 +261,7 @@ function validateInfomation(req, res, next) {
     //re-entered password
     const p2Name = req.body.password2;
     let p2Message = "";
-    if(pName !== p2Name) {
+    if (pName !== p2Name) {
         p2Message += "-Passwords do not match";
         error = true;
     }
@@ -259,13 +270,13 @@ function validateInfomation(req, res, next) {
     const email = req.body.email;
     const email2 = req.body.email2;
     let e2Message = "";
-    if(email !== email2) {
+    if (email !== email2) {
         e2Message += "-Emails do not match";
         error = true;
     }
 
     //redirect if needed
-    if(error) {
+    if (error) {
         req.flash('uMessage', uMessage);
         req.flash('pMessage', pMessage);
         req.flash('p2Message', p2Message);
@@ -279,15 +290,15 @@ function validateInfomation(req, res, next) {
 }
 
 function checkNotAuthenticated(req, res, next) {
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         return res.redirect(`/u/${req.user.username}`);
     }
-  
+
     next();
 }
 
 
-function getMailOptions(username='User', email, host, token) {
+function getMailOptions(username = 'User', email, host, token) {
     const protocol = `https`;
     const siteLink = `${protocol}:\/\/${host}`;
     const tokenLink = `${siteLink}\/signup\/verify?token=${token}`;
@@ -328,11 +339,11 @@ function getMailOptions(username='User', email, host, token) {
 
 
 function sendMail(mailOptions, email) {
-    if(process.env.NODE_ENV !== 'production' && !testEmail) {
+    if (process.env.NODE_ENV !== 'production' && !testEmail) {
         //development environment
         return console.log('Mail sent, make sure to actually send here');
     }
-    
+
     //otherwise, send mail
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     sgMail.send(mailOptions, function (err) {
